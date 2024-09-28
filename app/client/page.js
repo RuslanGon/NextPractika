711c1bfabef7a1f9c8cae47cdc70d8702fbf9b57
RuslanGon <ruslan441006@gmail.com>
2024-09-28 18:30:04 +0300

    wer

--- app/client/page.js
+++ app/client/page.js
@@ -1,6 +1,12 @@
+import Link from "next/link.js";
 
 export default function Client() {
   return (
-   <h1>Client Ruslan</h1>
+    <div>
+      <h1>Client Ruslan</h1>
+      <Link href="/">Main page</Link>
+      <br></br>
+      <Link href="client/1234">Client page</Link>
+    </div>
   );
 }
