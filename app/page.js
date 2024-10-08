00e2e4fd26ef6fc2cb575b8515711f3f0dfe9b4d
RuslanGon <ruslan441006@gmail.com>
2024-10-08 17:01:56 +0300

    wer

--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
+import Link from "next/link.js";
 
+ async function fetchPost () {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const result = await response.json()
+  return result
+}
 
-export default function Home() {
+export default async function Home() {
+  const posts = await fetchPost()
+
   return (
-   <h1>Hello Ruslan</h1>
+<div>
+<h1>Hello Ruslan </h1>
+{posts.map(el =>(
+  <div key={el.id} className="post">
+    <h2>{el.title}</h2>
+    <p>{el.body}</p>
+    <Link className="seemore" href={`/post/` + el.id}>See more</Link>
+  </div>
+))}
+</div>
   );
 }
