ef7f9ff6810190ef2bae4cf4b0c58ff47ff1802d
RuslanGon <ruslan441006@gmail.com>
2024-10-09 13:07:08 +0300

    wer

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,27 +1,35 @@
 import PostDetails from "@/app/components/PostDetails.js"
 
-export async function generateMetadata ({params, searchParams}) {
-  const post = await fetchPostById(params.id)
+export async function generateMetadata({ params }) {
+  const post = await fetchPostById(params.id);
   return {
     title: post.title,
-    description: post.body
-  }
+    description: post.body,
+  };
 }
 
-async function fetchPostById (id) {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id)
-    const result = await response.json()
-    return result
-  }
+async function fetchPostById(id) {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+  const result = await response.json();
+  return result;
+}
 
-const Post = async ({params: {id}}) => {
-    const posts = await fetchPostById(id)
+export async function generateStaticParams() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await response.json();
+
+  return posts.map(post => ({
+    id: post.id.toString(),
+  }));
+}
+
+const Post = async ({ params: { id } }) => {
+  const posts = await fetchPostById(id);
   return (
     <div className="post">
-        <PostDetails posts={posts}/>
-       
+      <PostDetails posts={posts} />
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
