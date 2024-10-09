090fde67588d86bd661ed1edd8cf63062239c72c
RuslanGon <ruslan441006@gmail.com>
2024-10-09 13:02:09 +0300

    wer

--- app/client/[clientId]/page.js
+++ app/client/[clientId]/page.js
@@ -1,8 +1,18 @@
-export default function ClientId({params}) {
-    return (
+export default function ClientId({ params }) {
+   return (
      <div>
-        <h1>ClientId page</h1>
-        <p>{params.clientId}</p>
+       <h1>ClientId page</h1>
+       <p>{params.clientId}</p>
      </div>
-    );
-  }
+   );
+ }
+ 
+
+ export async function generateStaticParams() {
+   const clients = [{ clientId: '1' }, { clientId: '2' }, { clientId: '3' }];
+ 
+   return clients.map(client => ({
+     clientId: client.clientId,
+   }));
+ }
+ 
